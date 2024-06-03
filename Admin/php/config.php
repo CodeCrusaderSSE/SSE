d642b2147cc57f1cc95cab98fdc7de751b125585
Costantino Di Bari <c.dibari14@studenti.uniba.it>
2024-06-03 18:06:13 +0200

    Fix of Password Management: Hardcoded Password in Admin/loginn.php

--- Admin/php/config.php
+++ Admin/php/config.php
@@ -3,5 +3,7 @@ return [
     'SMTP_USERNAME' => 'civicsense2019@gmail.com',
     'SMTP_PASSWORD' => 'c1v1csense2019',
     'GOOGLE_MAPS_API_KEY' => 'AIzaSyB7GIu4drL85xcaTdq8hAtRzVWjbKxs3NQ',
+    'ADMIN_EMAIL' => 'civicsense18@gmail.com',
+    'ADMIN_PASSWORD' => 'admin',
 ];
 ?>
--- Admin/php/loginn.php
+++ Admin/php/loginn.php
@@ -1,51 +1,44 @@
 <?php
-	//Recupero dati
-	if(isset($_POST['email']) && isset($_POST['password'])){
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		if($email == "civicsense18@gmail.com")
-		{
-			if($password == "admin")
-			{
-				echo 'Accesso consentito alla sezione riservata';
-			}
-			else
-			{
-				echo 'Accesso negato alla sezione riservata.La password � errata!';
-			}
-		}
-		else
-		{
-			
-			$conn = mysqli_connect ("localhost", "root", "","civicsense") or die ("Connessione non riuscita"); 
-			$stmt = $conn->prepare("SELECT * FROM team WHERE email_t = ?");
-			$stmt->bind_param("s",$email);
-			$stmt->execute();
-			$result = $stmt->get_result();
 
-			if (mysqli_num_rows($result) > 0) {
-	   
-	    		while($row = mysqli_fetch_assoc($result)) 
-				{
-					if($password != $row["password"] || $email != $row["email_t"])
-					{
-						//CODICE JAVASCRIPT
-						echo 'ATTENZIONE: La password o la email inserita non � corretta!';
-					}
-					else if ($password == $row["password"] || $email == $row["email_t"]){
-						echo 'Accesso consentito area riservata (TEAM)';
-					}
-			
-				}
-			}
-			$stmt->close();
-			$conn->close();
-		}
-	}
-	else{
-		echo 'Non esistono;';
-	}
-	
-	
-		
+$config = include('config.php');
+
+// Recupero dati
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $adminEmail = $config['ADMIN_EMAIL'];
+    $adminPassword = $config['ADMIN_PASSWORD'];
+
+    if ($email == $adminEmail) {
+        if ($password == $adminPassword) {
+            echo 'Accesso consentito alla sezione riservata';
+        } else {
+            echo 'Accesso negato alla sezione riservata. La password è errata!';
+        }
+    } else {
+        $conn = mysqli_connect("localhost", "root", "", "civicsense") or die("Connessione non riuscita");
+        $stmt = $conn->prepare("SELECT * FROM team WHERE email_t = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($password != $row["password"] || $email != $row["email_t"]) {
+                    // CODICE JAVASCRIPT
+                    echo 'ATTENZIONE: La password o la email inserita non è corretta!';
+                } else if ($password == $row["password"] || $email == $row["email_t"]) {
+                    echo 'Accesso consentito area riservata (TEAM)';
+                }
+            }
+        } else {
+            echo 'Non esistono utenti con questa email';
+        }
+        $stmt->close();
+        $conn->close();
+    }
+} else {
+    echo 'Non esistono;';
+}
 ?>
